c6ec828cae291efc65a666b31ec66fd2960797ed
Eelis van der Weegen <eelis@eelis.net>
2018-11-24 19:44:52 +0100

    Cache MathJax results.

--- MathJax.hs
+++ MathJax.hs
@@ -11,6 +11,8 @@ import System.IO (BufferMode(..), hGetLine, hPutStrLn, hSetBuffering)
 import Text.Regex (mkRegex, subRegex)
 import Prelude hiding ((++))
 import Util ((++))
+import qualified Data.Map as Map
+import Data.Map (Map)
 
 rmTrailingNewline :: Text -> Text
 rmTrailingNewline (Text.stripSuffix "\n" -> Just x) = x
@@ -18,11 +20,13 @@ rmTrailingNewline x = x
 
 type Renderer = String {- formula -} -> Bool {- inline -} -> Text
 
+data Input = Input { _formula :: String, _inline :: Bool }
+    deriving (Eq, Ord)
+
 makeRenderer :: IO Renderer
 makeRenderer = do
 
-    (Just stdinPipe, Just stdoutPipe, _, _) <- createProcess
-        (shell "/home/eelis/projects/c++/htmlstandard/cxxdraft-htmlgen/mathjax-batch")
+    (Just stdinPipe, Just stdoutPipe, _, _) <- createProcess (shell "./mathjax-batch")
             {std_in = CreatePipe, std_out = CreatePipe}
 
     hSetBuffering stdinPipe LineBuffering
@@ -38,21 +42,30 @@ makeRenderer = do
             more <- readResult
             return $ line ++ "\n" ++ more
 
-    mutex <- newMVar ()
+    mutex <- newMVar (Map.empty :: Map Input Text)
 
     return $ \formula inline -> unsafePerformIO $ do
-        takeMVar mutex
-        hPutStrLn stdinPipe formula
-        hPutStrLn stdinPipe (if inline then "INLINE" else "NONINLINE")
-        out <- readResult
-        putMVar mutex ()
-        return
-            $ Text.replace " focusable=\"false\"" ""
-            $ rmTrailingNewline -- Prevents artifacts in [rand.adapt.ibits]#4
-            $ Text.pack
-            $ rm " id=\"(MJXc|MathJax)-[0-9A-Za-z-]+\""
-            $ rm " style=\"\""
-            $ out
+        let input = Input formula inline
+        cache <- takeMVar mutex
+        (result, cache') <- case Map.lookup input cache of
+            Just output -> do
+                putStrLn "reusing"
+                return (output, cache)
+            Nothing -> do
+                hPutStrLn stdinPipe formula
+                hPutStrLn stdinPipe (if inline then "INLINE" else "NONINLINE")
+                rawResult <- readResult
+                let
+                  output
+                    = Text.replace " focusable=\"false\"" ""
+                    $ rmTrailingNewline -- Prevents artifacts in [rand.adapt.ibits]#4
+                    $ Text.pack
+                    $ rm " id=\"(MJXc|MathJax)-[0-9A-Za-z-]+\""
+                    $ rm " style=\"\""
+                    $ rawResult
+                return (output, Map.insert input output cache)
+        putMVar mutex cache'
+        return result
 
 render :: Renderer
 render = unsafePerformIO $ makeRenderer
