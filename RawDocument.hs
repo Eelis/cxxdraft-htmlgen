7577e5375464c88b481271f1f0c5195cc5760387
Eelis van der Weegen <eelis@eelis.net>
2020-09-26 10:32:11 +0200

    Improve plural grammarterm regex.

--- RawDocument.hs
+++ RawDocument.hs
@@ -262,7 +262,7 @@ parseFile macros =
 	. replace "\\hspace*" "\\hspace"
 	. replace "``" "“"
 	. replace "''" "”"
-	. textSubRegex (mkRegex "(\\grammarterm\\{[A-Za-z-]*\\})\\{s\\}") "\\1\\textit{s}"
+	. textSubRegex (mkRegex "(\\grammarterm\\{[A-Za-z-]*\\})(\\{s\\}|s)") "\\1\\textit{s}"
 		-- Mixing italic and upright looks okay in the PDF, but looks bad in browsers,
 		-- and our linkification makes clear enough that the plural 's' is not part
 		-- of the grammarterm.
