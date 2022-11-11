d34c99daedcba554ad4607f604ea83ce19d89b11
Eelis van der Weegen <eelis@eelis.net>
2022-11-11 14:49:08 +0100

    Recognize \operatorname.

--- RawDocument.hs
+++ RawDocument.hs
@@ -197,7 +197,7 @@ storeCmds =
 	, (1, "hspace footnote textit textrm textnormal texttt textbf ensuremath ref mbox " ++
 			"terminal literalterminal noncxxterminal renontermdef textsl textsc textsf text term " ++
 			"tcode noncxxtcode literaltcode footnotetext microtypesetup cline mathtt mathit mathrm mathsf " ++
-			"label newlength uline vspace value newcounter mathscr hyperref c uppercase iref " ++
+			"label newlength uline vspace value newcounter mathscr hyperref c uppercase iref operatorname " ++
 			"phantom sqrt ln emph minipage url indexescape changeglossnumformat textasciitilde " ++
 			"removedxref deprxref textsuperscript rlap mathrel mathbin nopnumdiffref color ucode uname")
 	, (2, "pnum definition addtocounter setcounter frac " ++
